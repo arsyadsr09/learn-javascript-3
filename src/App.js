bd838f5f3f9fd10f15cd23ef474f55c7fa1efe60
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2018-12-20 16:59:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import List from "./components/List";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
+
+  onChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form id="myDIV" className="header" onSubmit={this.onSubmit}>
+          <h1>My Todo List</h1>
+          <input value={this.state.term} onChange={this.onChange} />
+          <span className="addBtn">Submit</span>
+        </form>
+        <List items={this.state.items} />
       </div>
     );
   }
